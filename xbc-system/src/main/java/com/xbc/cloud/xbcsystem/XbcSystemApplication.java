b02341b9c154c1c32b41c41eb08fc3d93f31797c
weizeng <18550497663@sina.cn>
2019-04-29 19:28:38 +0800

    cloud 练手项目

---
+++ xbc-system/src/main/java/com/xbc/cloud/xbcsystem/XbcSystemApplication.java
@@ -0,0 +1,17 @@
+package com.xbc.cloud.xbcsystem;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+@MapperScan(basePackages = {"com.xbcraisins.wgr.xgrcommonservice.dao"})
+public class XbcSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XbcSystemApplication.class, args);
+    }
+
+}

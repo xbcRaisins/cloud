d87710265d981cb9ddcd71ce69b1ed7d31ec87c1
weizeng <18550497663@sina.cn>
2019-04-30 16:24:59 +0800

    spider images

--- xbc-web/src/main/java/com/xbc/web/xbcweb/spider/NewsCrawler.java
+++ xbc-web/src/main/java/com/xbc/web/xbcweb/spider/NewsCrawler.java
@@ -1,10 +1,11 @@
 package com.xbc.web.xbcweb.spider;
 
-
 import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
 import cn.edu.hfut.dmic.webcollector.model.Page;
 import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
 import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 描述：
@@ -14,6 +15,8 @@ import org.jsoup.nodes.Document;
  */
 public class NewsCrawler extends BreadthCrawler {
 
+    private static final Logger log = LoggerFactory.getLogger(NewsCrawler.class);
+
     public NewsCrawler(String crawlPath, boolean autoParse) {
         super(crawlPath, autoParse);
         /*start page*/
@@ -21,34 +24,27 @@ public class NewsCrawler extends BreadthCrawler {
         /*fetch url like http://news.hfut.edu.cn/show-xxxxxxhtml*/
         this.addRegex("http://news.163.com/*.*html");
         /*do not fetch jpg|png|gif*/
-        this.addRegex("-.*\\.(jpg|png|gif).*");
+        this.addRegex("+.*\\.(jpg|png|gif).*");
         /*do not fetch url contains #*/
         this.addRegex("-.*#.*");
     }
 
     public void visit(Page page, CrawlDatums next) {
         String url = page.url();
-        /*if page is news page*/
         if (page.matchUrl("http://news.163.com/*.*html")) {
-            /*we use jsoup to parse page*/
             Document doc = page.doc();
-
-            /*extract title and content of news by css selector*/
             String title = page.select("div[id=epContentLeft] > h1").first().text();
             String content = page.select("div[id=endText]").first().text();
 
-            System.out.println("URL:\n" + url);
-            System.out.println("title:\n" + title);
-            System.out.println("content:\n" + content);
+            log.info("URL:{}", url);
+            log.info("title:{}", title);
+            log.info("content:{}", content);
         }
     }
 
     public static void main(String[] args) throws Exception {
         NewsCrawler crawler = new NewsCrawler("crawl", true);
         crawler.setThreads(50);
-        //crawler.topN(100);
-        //crawler.setResumable(true);
-        /*start crawl with depth of 4*/
         crawler.start(4);
     }
 
